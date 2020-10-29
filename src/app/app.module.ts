63356988e2e34f5e223abd6465f9ca4ff9dbb932
Tim <43130816+DerStimmler@users.noreply.github.com>
2020-10-29 17:24:02 +0100

    added menu fixed layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GridComponent } from './grid/grid.component';
 import { SudokuComponent } from './sudoku/sudoku.component';
 import {FormsModule} from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatCardModule} from '@angular/material/card';
+import { MenuComponent } from './menu/menu.component';
+import {CommonModule} from '@angular/common';
 
 @NgModule({
   declarations: [
     AppComponent,
     GridComponent,
-    SudokuComponent
+    SudokuComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatCardModule,
+    CommonModule
   ],
   providers: [],
   bootstrap: [AppComponent],
-  exports: [GridComponent, SudokuComponent]
+  exports: [GridComponent, SudokuComponent, MenuComponent]
 })
 export class AppModule { }
